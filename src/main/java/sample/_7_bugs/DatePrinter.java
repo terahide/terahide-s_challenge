2f9565a5913b183872560a1b742baf2f76595499
hideki.terashima <hideki.terashima@bigtreetc.com>
2021-05-11 18:43:28 +0900

    resolve 2 SimpleDateFormatはスレッドセーフじゃないのでローカル変数へ

--- src/main/java/sample/_7_bugs/DatePrinter.java
+++ src/main/java/sample/_7_bugs/DatePrinter.java
@@ -8,13 +8,12 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class DatePrinter {
-    final static SimpleDateFormat formatter = new SimpleDateFormat("yyyy.MM.dd");
 
     public static void main(String[] args) throws Exception{
         StringBuilder sb = new StringBuilder();
         List<Thread> l = IntStream.range(0,10000)
-                .mapToObj( i -> formatter.format(new Date()) )
                 .filter(s -> s == "2021.05.11") // 前提：今日は 2021.05.11 とする
+                .mapToObj(DatePrinter::dateFormat)
                 .map( s -> new Thread(()->{
                     sb.append(s);
                     sb.append("\n");
@@ -26,6 +25,11 @@ public class DatePrinter {
 
         System.out.println(sb);
         System.out.println("finish");
+    }
+
+    static String dateFormat(int i){
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy.MM.dd");
+        return formatter.format(new Date());
     }
 
     static void joinThread(Thread t){
